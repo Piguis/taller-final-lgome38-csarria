da1c4c4f317132a6819988b48ae0239d37576365
Piguis <65557167+csarria@users.noreply.github.com>
2024-08-16 16:33:41 -0500

    Se genera parte de la estructura del menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,36 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
 
-        System.out.println("Hola Mundo leonardo");
+
+
+
+
+        System.out.println("------------------Menu Taller--------------------");
+        System.out.println("1. Datos primitivos");
+        System.out.println("2. String");
+        System.out.println("3. Constante");
+        System.out.println("4. Tipo de Operadores");
+        System.out.println("5. Condicional IF, ELSE, IF ELSE");
+
+
+
+
+
+        System.out.println("-------------------------------------------------");
+        System.out.println("Ingresa un numero para ver el contenido: ");
+        Scanner valorMenu = new Scanner(System.in);
+        byte numeroValor = valorMenu.nextByte();
+
+
+
+
+
+
 
 
     }
