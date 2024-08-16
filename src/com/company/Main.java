597013541fdc849a4a9c6a72288044fd8b6dd294
lgome38 <leonardo.0624@gmail.com>
2024-08-16 16:54:20 -0500

    complete tabla menu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,14 +11,17 @@ public class Main {
 
 
         System.out.println("------------------Menu Taller--------------------");
-        System.out.println("1. Datos primitivos");
-        System.out.println("2. String");
-        System.out.println("3. Constante");
-        System.out.println("4. Tipo de Operadores");
-        System.out.println("5. Condicional IF, ELSE, IF ELSE");
-
-
-
+        System.out.println("1. Datos primitivos.");
+        System.out.println("2. String.");
+        System.out.println("3. Constante.");
+        System.out.println("4. Tipo de Operadores.");
+        System.out.println("5. Condicional IF, ELSE, IF ELSE.");
+        System.out.println("6. Condicional Switch.);
+        System.out.println("7. Condicional ternaria.");
+        System.out.println("8. Bucle Do WHILE.");
+        System.out.println("9. Bucle WHILE.");
+        System.out.println("10. Bucle FOR.");
+        System.out.println("0. Salir");
 
 
         System.out.println("-------------------------------------------------");
